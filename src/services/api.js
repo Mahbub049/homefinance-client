68be509917cdc4224e80dd89cd05621b1d459b47
Mahbub049 <mahbubsarwar49@gmail.com>
2026-05-11 01:47:28 +0600

    api changed

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,15 @@ import { getToken } from "./authStorage";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
-  // baseURL: "http://localhost:5000",
 });
 
 api.interceptors.request.use((config) => {
   const token = getToken();
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
